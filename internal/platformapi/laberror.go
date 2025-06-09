39ee8fb4598c19166b37b53cbcd603a4b6676849
Aaron Goodfellow <agoodfellow@nowsecure.com>
2025-06-09 15:51:38 -0400

    assert that LabRouteError impls Error

--- internal/platformapi/laberror.go
+++ internal/platformapi/laberror.go
@@ -2,7 +2,10 @@ package platformapi
 
 import (
 	"fmt"
+	nserrors "github.com/nowsecure/nowsecure-ci/internal/errors"
 )
+
+var _ nserrors.CIError = (*LabRouteError)(nil)
 
 func (w *LabRouteError) Error() string {
 	return fmt.Sprintf("HTTP %s - %s: %s", *w.Status, *w.Name, *w.Message)
