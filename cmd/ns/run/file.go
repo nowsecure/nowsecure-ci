a534d87662737cee6c909d4721e187b32933e52c
Aaron Goodfellow <agoodfellow@nowsecure.com>
2025-06-23 13:33:02 -0400

    remove unecessary message

--- cmd/ns/run/file.go
+++ cmd/ns/run/file.go
@@ -56,7 +56,7 @@ func FileCommand(v *viper.Viper) *cobra.Command {
 			if err != nil {
 				return err
 			}
-			log.Info().Str("URL", fmt.Sprintf("Running assessment with URL: %s/app/%s/assessment/%s", config.UIHost, buildResponse.Application, buildResponse.Ref)).Msg("Assessment URL")
+			log.Info().Str("URL", fmt.Sprintf("%s/app/%s/assessment/%s", config.UIHost, buildResponse.Application, buildResponse.Ref)).Msg("Assessment URL")
 
 			if config.PollForMinutes <= 0 {
 				log.Info().Msg("Succeeded")
