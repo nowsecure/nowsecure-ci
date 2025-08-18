10284d0c370f326b8d05a733739f3dce0c5f8e37
Aaron Goodfellow <amgoodfellow@protonmail.com>
2025-08-18 13:28:36 -0400

    fix(PI-4997): assessment failure path fixes
    Co-authored-by: Aaron Goodfellow <agoodfellow@nowsecure.com>

--- cmd/ns/run/run.go
+++ cmd/ns/run/run.go
@@ -86,11 +86,21 @@ func pollForResults(ctx context.Context, client *platformapi.ClientWithResponses
 
 			var completed platformapi.GetAppPlatformPackageAssessmentTask2XXTaskStatus = "completed"
 			var failed platformapi.GetAppPlatformPackageAssessmentTask2XXTaskStatus = "failed"
-			// A 2XX indicates a finalized assessment but not necessarily the findings or score being calculated
-			if resp.StatusCode() == 200 && resp.JSON2XX.AdjustedScore != nil {
-				if *resp.JSON2XX.TaskStatus == completed || *resp.JSON2XX.TaskStatus == failed {
+			if resp.StatusCode() == 200 {
+				// A 2XX indicates a finalized assessment but not necessarily the findings or score being calculated
+				if *resp.JSON2XX.TaskStatus == completed && resp.JSON2XX.AdjustedScore != nil {
 					zerolog.Ctx(ctx).Debug().Msg("Polling complete")
 					return resp, nil
+				}
+
+				errorCode := "nil"
+				if resp.JSON2XX.TaskErrorCode != nil {
+					errorCode = *resp.JSON2XX.TaskErrorCode
+				}
+
+				if *resp.JSON2XX.TaskStatus == failed {
+					zerolog.Ctx(ctx).Debug().Msg("Polling complete")
+					return nil, fmt.Errorf("assessment failed with %v error code", errorCode)
 				}
 			}
 		}
