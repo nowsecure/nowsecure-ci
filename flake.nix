00548ff8ef94e0524119d7189d5423be74559619
Aaron Goodfellow <agoodfellow@nowsecure.com>
2025-08-21 13:55:11 -0400

    ci: update publish step to use goreleaser
    - releases will now have tgz archives in the format: ns_darwin-arm64.tgz
    - updates nix flake to include linting and formatting shell script helpers

--- flake.nix
+++ flake.nix
@@ -3,41 +3,93 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  outputs = { self, nixpkgs, ... }@inputs:
+  outputs =
+    { self, nixpkgs, ... }@inputs:
     let
       name = "ns-ci";
 
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "aarch64-linux" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "aarch64-linux"
+      ];
 
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system; };
+            system = system;
+          }
+        );
 
       vendorHash = nixpkgs.lib.fakeHash;
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = [
-            pkgs.go_1_24
-            pkgs.gopls
-            pkgs.golangci-lint
-            pkgs.gci
+    in
+    {
+      packages = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.buildGo124Module {
+            inherit name vendorHash;
+            src = ./.;
+            doCheck = true;
+          };
+          format = pkgs.writeShellApplication {
+            name = "format";
+            runtimeInputs = [
+              pkgs.gci
+            ];
+            text = ''
+              gci write -s standard -s default -s localmodule ./
+            '';
+          };
+          checks = pkgs.writeShellApplication {
+            name = "checks";
+            runtimeInputs = [
+              pkgs.actionlint
+              pkgs.golangci-lint
+              pkgs.typos
+            ];
+            text = ''
+              set +e
+              echo "GolangCI Lint:"
+              golangci-lint run
+              echo "Action Lint:"
+              actionlint
+              echo "Typos:"
+              typos && echo "0 issues."
+            '';
+          };
+        }
+      );
 
-            pkgs.oapi-codegen
-            pkgs.cobra-cli
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default = pkgs.mkShell {
+            packages = [
+              # Development dependancies
+              pkgs.cobra-cli
+              pkgs.go_1_24
+              pkgs.gopls
+              pkgs.oapi-codegen
 
-            pkgs.nixpkgs-fmt
-          ];
-        };
-      });
+              # Linting helpers
+              pkgs.actionlint
+              pkgs.gci
+              pkgs.golangci-lint
+              pkgs.typos
 
-      packages = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.buildGo124Module {
-          inherit name vendorHash;
-          src = ./.;
-          doCheck = true;
-        };
+              # Shell script helpers
+              self.packages.${system}.checks
+              self.packages.${system}.format
 
-      });
+              # Other
+              pkgs.nixpkgs-fmt
+            ];
+          };
+        }
+      );
     };
 }
